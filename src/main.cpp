6cbd553ae934b83238f766e29696d3c69b405bd3
Jacques Moati <jacques@moati.net>
2020-06-23 07:21:01 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,34 @@
 #include <FastLED.h>
 #include <ArduinoJson.h>
 
-#define LED_PIN 5
-#define NUM_LEDS 84
+#define LED_PIN 23
+#define NUM_LEDS 48
 #define BRIGHTNESS 64
 #define FADE 42
 
 CRGB leds[NUM_LEDS];
 
-String devs_names[5] =  {
+String devs_names[4] =  {
   "BRO",
   "GDE",
   "JMO",
-  "TAL",
-  "LPH"
+  "MJO",
 };
 
-CRGB colors[5] =  {
+CRGB colors[4] =  {
   CRGB(0, 200, 0),
   CRGB(0, 0, 200),
   CRGB(200, 0, 0),
   CRGB(255, 140, 0),
-  CRGB(50, 0, 255),
 };
 
 StaticJsonDocument<1024> reviews;
   
-int pixels[5][12] = {
-  {35, 47, 49, 61, 63, 75, 77, 5,  7,  19, 21, 33},
-  {32, 36, 46, 50, 60, 64, 74, 78, 4,  8,  18, 22},
-  {23, 31, 37, 45, 51, 59, 65, 73, 79, 3,   9, 17},
-  {16, 24, 30, 38, 44, 52, 58, 66, 72, 80,  2, 10},
-  {11, 15, 25, 29, 39, 43, 53, 57, 67, 71, 81,  1}
+int pixels[4][12] = {
+  {12, 19, 20, 27, 28, 35, 36, 43, 44, 3, 4, 11},
+  {10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 2, 5},
+  {6, 9, 14, 17, 22, 25, 30, 33, 38, 41, 46, 1},
+  {0, 7, 8, 15, 16, 23, 24, 31, 32, 39, 40, 47},
 };
 
 int computeSpeed(int mrCount) {
@@ -45,40 +42,41 @@ void setup() {
 }
 
 void loop() {
-    for (int tempo=0; tempo<14400; ++tempo) { // 12 * 120
-        for (int x_pos=0; x_pos<12; ++x_pos) {
-          if(Serial.available() > 0) {
-            StaticJsonDocument<1024> json;
-            String data = Serial.readStringUntil('\n');
-            DeserializationError error = deserializeJson(json, data);
+  for (int tempo=0; tempo<14400; ++tempo) { // 12 * 120,
+    for (int x_pos=0; x_pos<12; ++x_pos) {
+      for (int element=0; element<4; ++element) {
+        if(Serial.available() > 0) {
+          StaticJsonDocument<1024> json;
+          String data = Serial.readStringUntil('\n');
+          DeserializationError error = deserializeJson(json, data);
 
-            if (error) {
-              return;
-            }
+          if (error) {
+            return;
+          }
             
-            reviews = json;
-          }
-          for (int element=0; element<5; ++element) {
-            if (element < reviews.size()) {
-              StaticJsonDocument<1024> review = reviews.getElement(element); 
+          reviews = json;
+        }
 
-              if ((tempo/computeSpeed(review["count"])%12 == x_pos)) {
-                for (int dev_pos = 0; dev_pos <= 5; ++dev_pos) {
-                  if (devs_names[dev_pos] == review["who"]) {
-                    leds[pixels[element][x_pos]] = colors[dev_pos];              
-                  }
-                }
-              } else {
-                leds[pixels[element][x_pos]].fadeToBlackBy(FADE);
+        if (element < reviews.size()) {
+          StaticJsonDocument<1024> review = reviews.getElement(element); 
+
+          if ((tempo/computeSpeed(review["count"])%12 == x_pos)) {
+            for (int dev_pos = 0; dev_pos <= 4; ++dev_pos) {
+              if (devs_names[dev_pos] == review["who"]) {
+                leds[pixels[element][x_pos]] = colors[dev_pos];              
               }
-            } else {
-                leds[pixels[element][x_pos]].fadeToBlackBy(FADE);
             }
-
+          } else {
+            leds[pixels[element][x_pos]].fadeToBlackBy(FADE);
           }
+        } else {
+            leds[pixels[element][x_pos]].fadeToBlackBy(FADE);
+        }
 
-        FastLED.show();
-        delay(1);
       }
+
+      FastLED.show();
+      delay(1);
+    }
   }
 }
